01e4477ec67d958568ffc90ca40e842860e3ba93
GABRIEL SANTA ROSA CORTEZ <gabriel.santa@academico.ifrn.edu.br>
2023-04-11 08:13:33 -0300

    Metodo Creatre feito

--- WebApplication2/Controllers/CategoriasController.cs
+++ WebApplication2/Controllers/CategoriasController.cs
@@ -25,5 +25,10 @@ namespace WebApplication2.Controllers
             return View(categorias);
         }
 
+        public ActionResult Create()
+        {
+            return View();
+        }
+
     }
 }
