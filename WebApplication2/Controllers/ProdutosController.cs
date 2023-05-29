3365d3c2cd987347f4967b0484c0fa11a3f2a222
GABRIEL SANTA ROSA CORTEZ <gabriel.santa@academico.ifrn.edu.br>
2023-05-29 15:40:02 -0300

    create

--- WebApplication2/Controllers/ProdutosController.cs
+++ WebApplication2/Controllers/ProdutosController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using WebApplication2.Models;
+using System.Data.Entity;
 
 namespace WebApplication2.Controllers
 {
@@ -13,7 +14,10 @@ namespace WebApplication2.Controllers
         // GET: Produtos
         public ActionResult Index()
         {
-            return View(context.Produtos.OrderBy(c => c.Nome));
+            var produtos =
+            context.Produtos.Include(c => c.Categoria).Include(f => f.Fabricante).
+            OrderBy(n => n.Nome);
+            return View(produtos);
         }
 
         // GET: Produtos/Details/5
@@ -25,16 +29,23 @@ namespace WebApplication2.Controllers
         // GET: Produtos/Create
         public ActionResult Create()
         {
+            ViewBag.CategoriaId = new SelectList(context.Categorias.OrderBy(b => b.Nome),
+            "CategoriaId", "Nome");
+            ViewBag.FabricanteId = new SelectList(context.Fabricantes.OrderBy(b => b.Nome),
+            "FabricanteId", "Nome");
+
             return View();
         }
 
         // POST: Produtos/Create
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        public ActionResult Create(Produto produto)
         {
             try
             {
                 // TODO: Add insert logic here
+                context.Produtos.Add(produto);
+                context.SaveChanges();
 
                 return RedirectToAction("Index");
             }
